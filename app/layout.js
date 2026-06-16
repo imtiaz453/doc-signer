0c081514763837491fe3726f3d408d9915519311
imtiaz453 <143900031+imtiaz453@users.noreply.github.com>
2026-06-16 10:08:05 +0500

    various fixes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css';
 import Provider from '@/components/SessionProvider';
 import ServiceWorkerRegister from '@/components/ServiceWorkerRegister';
-import InstallPrompt from '@/components/InstallPrompt';
 
 export const metadata = {
   title: 'DocSigner - PDF Signing & Stamping',
@@ -30,7 +29,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <ServiceWorkerRegister />
-        <InstallPrompt />
         <Provider>{children}</Provider>
       </body>
     </html>
